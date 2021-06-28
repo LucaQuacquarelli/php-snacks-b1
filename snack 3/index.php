73cb394e3778eae44ea7beb08a66dd4a123e7360
LucaQuacquarelli <lucaq82@libero.it>
2021-06-28 19:39:01 +0200

    snack 3

---
+++ snack 3/index.php
@@ -0,0 +1,14 @@
+<?php 
+    $numbers = [];
+
+    while (count($numbers) < 15) {
+        $number = rand(1, 100);
+    
+        if (!in_array($number, $numbers)) {
+            $numbers[] = $number;
+        }
+    }
+    var_dump($numbers);
+?>
+
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
